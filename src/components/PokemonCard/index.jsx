2b49c5ff275c12d771dab35ea2f5ca6d4afeac18
Gabrielporfiri0 <g.porfirio502@gmail.com>
2025-05-23 09:31:35 -0400

    Estilização

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -1,32 +1,50 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-export default function PokemonCard({name, image}) {
+export default function PokemonCard({ name, image }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card
+      sx={{
+        width: 150,
+        height: 200,
+        margin: 2,
+        borderRadius: 2,
+        boxShadow: 3,
+        backgroundColor: '#ffffff',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        transition: 'transform 0.2s ease',
+        '&:hover': {
+          transform: 'scale(1.05)',
+        },
+      }}
+    >
       <CardMedia
-        sx={{ height: 140 }}
-        image={ image }
-        title="green iguana"
+        component="img"
+        image={image}
+        alt={name}
+        sx={{
+          height: 96,
+          width: 96,
+          objectFit: 'contain',
+          marginTop: 2,
+        }}
       />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+      <CardContent sx={{ padding: 1 }}>
+        <Typography
+          variant="subtitle1"
+          component="div"
+          textAlign="center"
+          textTransform="capitalize"
+        >
           {name}
         </Typography>
-        {/*<Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>*/}
       </CardContent>
-     {/*<CardActions>
-       } <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>*/}
     </Card>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar';
 import PokemonCard from '../components/PokemonCard';
 import { Container, Grid } from '@mui/material';
 import axios from 'axios';
 
 export const Home = () => {
-    const [pokemons, setPokemons] = React.useState([]);
-    useEffect   (() => {
-        getPokemons();
-    },[]);
-    const getPokemons = () =>{
-        var endpoints = []
-        for(var i=1; i<=50; i++){
-            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
-        }
-        var response = axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
-        console.log(response);
-    };
-    
-    return (
-        <div>
-            <Navbar />
-            <Container maxWidth="false">
-                <Grid container>
-                    {pokemons.map((pokemon, key)=><Grid item xs={3} key={key}>
-                        <PokemonCard name={pokemon.data.name}  image={pokemon.data.sprites.front_default}  />
-                     </Grid>
-                    )}
-                    
+  const [pokemons, setPokemons] = useState([]);
 
-                </Grid>
-             </Container>
-        </div>
-    );
-}
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    const endpoints = [];
+    for (let i = 1; i <= 50; i++) {
+      endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`);
+    }
+
+    try {
+      const responses = await axios.all(endpoints.map((endpoint) => axios.get(endpoint)));
+      setPokemons(responses);
+    } catch (error) {
+      console.error('Erro ao buscar os pokémons:', error);
+    }
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <Container maxWidth="false">
+        <Grid container spacing={2}>
+          {pokemons.map((pokemon, key) => (
+            <Grid item xs={3} key={key}>
+              <PokemonCard
+                name={pokemon.data.name}
+                image={pokemon.data.sprites.front_default} // <- CORREÇÃO AQUI
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
